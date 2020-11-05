0fb77062a2f70d72a6c718974c2ff4490eb61d0e
diego <webdiegomassari@gmail.com>
2020-11-05 12:59:50 +1100

    fix init function

--- script.js
+++ script.js
@@ -34,10 +34,10 @@ const switchPlayer= ()=>{
 
 //STARTING CONDITION
 const init = ()=>{
-const scores = [ 0, 0]
-let currentScore = 0;
-let activePlayer= 0;
-let playing = true;
+scores = [ 0, 0]
+currentScore = 0;
+ activePlayer= 0;
+ playing = true;
 
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -122,8 +122,4 @@ btnHold.addEventListener('click', ()=>{
 
 //New game 
 
-btnNew.addEventListener('click', ()=>{
-
-  init()
-
-})
+btnNew.addEventListener('click', init) 
